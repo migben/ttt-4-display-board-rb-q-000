9709e049c09a70374b71608c1103185d2d6f202b
Miguel Ben <codexheartdevs@gmail.com>
2019-11-19 05:31:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(arr)
-  arr
+  puts " " + board[0] + " | " + board[1] + " | " + board[2] + " "
+  
 end
